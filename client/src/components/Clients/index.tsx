cba4783c8925f7d80378f9ff31f0d152c2deb581
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-18 03:53:18 +0600

    succesfully fetching data from graphql

--- client/src/components/Clients/index.tsx
+++ client/src/components/Clients/index.tsx
@@ -1,31 +1,31 @@
-import { gql } from "@apollo/client";
-import { client } from "pages/_app";
-
 const Clients = ({ clients }: any) => {
   console.log(clients);
 
-  return <div></div>;
+  return (
+    <div>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        {clients.map((client: any, id: number) => (
+          <tbody>
+            <tr>
+              <td>{id + 1}</td>
+              <td>{client?.name}</td>
+              <td>{client?.email}</td>
+              <td>{client?.phone}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+  );
 };
 
 export default Clients;
-
-export async function getServerSideProps() {
-  const { data } = await client.query({
-    query: gql`
-      query Clients {
-        clients {
-          id
-          name
-          email
-          phone
-        }
-      }
-    `,
-  });
-
-  return {
-    props: {
-      clients: data.clients,
-    },
-  };
-}
